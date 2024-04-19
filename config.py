849ca0a8b15b7c19ee10f19131c81a21ed5b104e
rain <128030543+rain305f@users.noreply.github.com>
2024-04-19 10:03:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-VICUNA_PATH = "/data/xiaogeng_liu/modellib/vicuna-13b-v1.5"
-LLAMA_PATH = "/data/xiaogeng_liu/JailbreakBench/model/Llama-2-7b-chat-hf"
+VICUNA_PATH = ""
+LLAMA_PATH = ""
 
 DEFENSE_TEMP = 1
 TARGET_TEMP = 0
 DEFENSE_TOP_P = 0.9
 TARGET_TOP_P = 1
-OPENAI_API_KEY = "aca9e0abe5b04da395b2cd340fc2e084"
+OPENAI_API_KEY = ""
